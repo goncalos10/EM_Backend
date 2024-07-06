9b7f7415fedd37814ae3c3422a5b40c988132e1e
goncalos10 <a18847@alunos.ipca.pt>
2024-07-06 15:40:22 +0100

    login

--- src/resources/login/loginController.js
+++ src/resources/login/loginController.js
@@ -9,12 +9,13 @@ class LoginController {
         let loginQuery = this.loginQuery
         return async function (req, res) {
             let { email, password } = req.body
-            
+
             const user = await loginQuery.execute({ email, password })
 
             if (!user.success) {
                 return res.status(400).json({ error: user.error })
             }
+
             return res.status(200).json(user)
         }
     }
--- src/resources/login/loginQuery.js
+++ src/resources/login/loginQuery.js
@@ -10,17 +10,17 @@ class LoginQuery {
         try {
 
             const userlogin = await this.userRepository.findByEmail(email)
-            if(!userlogin) {
+            if (!userlogin) {
                 throw new Error('Email or password are wrong')
             }
 
             const validatePassword = await utils.comparePassword(password, userlogin.password)
-            if(!validatePassword) {
+            if (!validatePassword) {
                 throw new Error('Email or password are wrong')
             }
             console.log(userlogin)
             const token = utils.generateJWT(userlogin.userid, userlogin.name, userlogin.email, userlogin.roleid)
-            return { success: true, data: token }
+            return { success: true, data: { token, userid: userlogin.userid, email: userlogin.email, roleid: userlogin.roleid, name: userlogin.name } }
 
         } catch (error) {
             return { success: false, error: error.message }
