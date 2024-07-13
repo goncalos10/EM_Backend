4fb638190d8d2dc7338e4fc6acc91da548dae55f
goncalos10 <a18847@alunos.ipca.pt>
2024-07-13 18:28:40 +0100

    variable

--- src/resources/getMyFunerarias/getMyFunerariasQuery.js
+++ src/resources/getMyFunerarias/getMyFunerariasQuery.js
@@ -9,7 +9,7 @@ class GetMyFunerariasQuery {
     async execute(user) {
         try {
             if (user.roleid === 1) {
-                let funerariasUser = (await this.funerariaRepository.findAllFunerarias()) || []
+                const funerariasUser = (await this.funerariaRepository.findAllFunerarias()) || []
                 return { success: true, data: funerariasUser }
             }
             const funerariasUser = (await this.funerariaRepository.findFunerariasByUserID(user.userid)) || []
